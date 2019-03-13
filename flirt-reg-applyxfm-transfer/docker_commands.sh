90b5f26b67b119b9d2aaa323341c3b88b4797131
Katie Surrence <tiburona@gmail.com>
2019-03-13 15:32:17 -0400

    removed local docker commands

--- flirt-reg-applyxfm-transfer/docker_commands.sh
+++
@@ -1,6 +0,0 @@
-docker stop new-container && docker rm new-container
-docker image rm friendlyhello
-docker build -t friendlyhello .
-docker create -it --name new-container friendlyhello
-docker start new-container
-docker exec -it new-container bash
